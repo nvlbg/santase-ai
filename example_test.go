8b680ff8fe4da371ada173694278907ae00c5375
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2019-01-01 18:45:36 +0200

    Add example test

---
+++ example_test.go
@@ -0,0 +1,34 @@
+package santase
+
+import "fmt"
+
+func ExampleGame() {
+	// create initial hand for the ai
+	hand := NewHand()
+	hand.AddCard(NewCard(Nine, Diamonds))
+	hand.AddCard(NewCard(King, Spades))
+	hand.AddCard(NewCard(Queen, Diamonds))
+	hand.AddCard(NewCard(Nine, Spades))
+	hand.AddCard(NewCard(Ace, Spades))
+	hand.AddCard(NewCard(Ten, Hearts))
+
+	// create trump card for the game
+	trumpCard := NewCard(Ten, Clubs)
+
+	// is the opponent first to move
+	isOpponentMove := true
+
+	// create a game
+	game := CreateGame(hand, trumpCard, isOpponentMove)
+
+	// update the game with the move the opponent makes
+	game.UpdateOpponentMove(NewMove(NewCard(Nine, Hearts)))
+
+	// start the AI
+	move := game.GetMove()
+
+	fmt.Println(move)
+
+	// finish the first round by updating what card the AI draws
+	game.UpdateDrawnCard(NewCard(Jack, Hearts))
+}
