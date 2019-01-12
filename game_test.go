05e3d059da2c3199343e7e04c4453807e6974fb6
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2019-01-12 16:40:15 +0200

    Change module path

--- game_test.go
+++ game_test.go
@@ -1,8 +1,9 @@
 package santase
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewGame(t *testing.T) {
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module santase
+module github.com/nvlbg/santase-ai
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
